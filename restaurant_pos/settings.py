c5f3c5194b02db65fa5063318f80061f61965463
Mithun Mohan <mithunmohan9633@users.noreply.github.com>
2026-05-07 21:57:58 +0530

    Switch to PostgreSQL support for production

--- restaurant_pos/settings.py
+++ restaurant_pos/settings.py
@@ -1,3 +1,4 @@
+import dj_database_url
 """
 Django settings for restaurant_pos project.
 
@@ -75,11 +76,14 @@ WSGI_APPLICATION = 'restaurant_pos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+        conn_max_age=600
+    )
+}
+
 }
 
 
---
+++ update_db.py
@@ -0,0 +1,23 @@
+﻿import os
+import re
+path = 'restaurant_pos/settings.py'
+with open(path, 'r') as f:
+    content = f.read()
+
+if 'import dj_database_url' not in content:
+    content = 'import dj_database_url\n' + content
+
+db_config = """
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+        conn_max_age=600
+    )
+}
+"""
+
+content = re.sub(r'DATABASES = \{.*?\}', db_config, content, flags=re.DOTALL)
+
+with open(path, 'w') as f:
+    f.write(content)
+print('Updated settings.py for PostgreSQL')
